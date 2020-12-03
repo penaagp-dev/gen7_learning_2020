3683968a7468c8c3fb84eb0b9e9f613e9cacd046
rahyu21 <ayurahayusiti13@gmail.com>
2020-12-03 23:05:47 +0800

    add: rahayu

---
+++ rahayu.py
@@ -0,0 +1,40 @@
+def batas ():
+	print ("******************************************************************************")
+while True :
+	batas ()
+	print ("\t Aplikasi Kekuatan Karakter Zodiak")
+	batas ()
+	a = input("Masukan Zodiak kalian = ")
+	if (a == "aquarius") :\
+		print ("Kekuatan Karakter Zodiak adalah progresif, original dan mandiri.")
+	elif (a == "pisces") :\
+		print ("Kekuatan Karakter Zodiak adalah berbelas kasih dan intuitif.")
+	elif (a == "aries") :\
+		print ("Kekuatan Karakter Zodiak adalah berani dan bersemangat.")
+	elif (a == "taurus") :\
+		print ("Kekuatan Karakter Zodiak adalah bisa diandalkan dan setia.")
+	elif (a == "gemini") :\
+		print ("Kekuatan Karakter Zodiak adalah pribadi yang penyayang dan penuh rasa penasaran.")
+	elif (a == "cancer") :\
+		print ("Kekuatan Karakter Zodiak adalah sangat imajinatif, setia dan simpatik.")
+	elif (a == "leo") :\
+		print ("Kekuatan Karakter Zodiak adalah kreatif dan berhati hangat.")
+	elif (a == "virgo") :\
+		print ("Kekuatan Karakter Zodiak adalah setia dan pekerja keras.")
+	elif (a == "libra") :\
+		print ("Kekuatan Karakter Zodiak adalah bersikap kooperatif dan berpikir adil.")
+	elif (a == "scropio") :\
+		print ("Kekuatan Karakter Zodiak adalah berani dan banyak akal.")
+	elif (a == "sagitarius") :\
+		print ("Kekuatan Karakter Zodiak adalah idealis dan memiliki selera humor tinggi.")
+	elif (a == "capricorn") :\
+		print ("Kekuatan Karakter Zodiak adalah bertanggung jawab.")
+	else :
+		print ("perintah tidak terdaftar")
+	b = input("Apakah anda merasa puas dengan jawabannya?? (ya/tidak)")
+	if (b == "tidak") :\
+			True
+	else : 
+			batas()
+			print ("Senang bisa membantu")
+			break
