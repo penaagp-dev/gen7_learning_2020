65978a8f6ca4d3e204a32d30a4ab2ae95960f387
unknown <ketutyulianti999@gmail.com>
2020-12-04 20:28:41 +0700

    add: Yulianti

---
+++ Yulianti.py
@@ -0,0 +1,21 @@
+#penjumlahan
+
+a=input (" nilai a = ")
+b=input (" nilai b = ")
+c=a+b
+print "\n""hasil penjumlahan = ",c
+
+#pengurangan
+
+d=a-b
+print "\n""hasil pengurangan = ",d
+
+#perkalian
+
+e=a*b
+print "\n""hasil perkalian = ",e
+
+#pembagian
+
+f=a/b
+print "\n""hasil pembagian = ",f
